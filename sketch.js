06a0a61cfaa5d0b2a4345efa19bd7302de453f58
David Gonzalez <davidgonzalez5910@gmail.com>
2023-02-08 19:08:47 -0500

    Mouse trace function added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
-for(let i = 0; i < 256; i++) {
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'mini-div');
-
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    newDiv.setAttribute("style", `background-color: #${randomColor};`);
-    
-    let dashBoard = document.querySelector('#grid-container');
-    dashBoard.appendChild(newDiv);
+function gridGenerator() {
+    for(let i = 0; i < 256; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'mini-div');
+        
+        let dashBoard = document.querySelector('#grid-container');
+        dashBoard.appendChild(newDiv);
+    }
 }
+
+gridGenerator();
+
+let allDivs = document.querySelectorAll('.mini-div');
+allDivs.forEach(miniDiv => miniDiv.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = "purple";
+}));
