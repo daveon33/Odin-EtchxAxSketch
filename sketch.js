81e7aa0cabce89ffbc1dfa73300da33c8e745886
David Gonzalez <davidgonzalez5910@gmail.com>
2023-02-10 10:54:53 -0500

    Improved grid generator to it to create different grids depending on the size value

--- sketch.js
+++ sketch.js
@@ -1,16 +1,25 @@
-function gridGenerator() {
-    for(let i = 0; i < 256; i++) {
+function gridGenerator(size = 16) {
+    for(let i = 0; i < (size * size); i++) {
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', 'mini-div');
+
+        let reshapeGrid = size / 480;
+        newDiv.setAttribute('height', `${reshapeGrid}`);
+        newDiv.setAttribute('width', `${reshapeGrid}`);
         
         let dashBoard = document.querySelector('#sketcher');
         dashBoard.appendChild(newDiv);
     }
 }
 
-gridGenerator();
-
-let allDivs = document.querySelectorAll('.mini-div');
-allDivs.forEach(miniDiv => miniDiv.addEventListener('mouseover', (e) => {
+function changeColor() {
+    let allDivs = document.querySelectorAll('.mini-div');
+    allDivs.forEach(miniDiv => miniDiv.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = "purple";
-}));
+    }));
+}
+
+
+
+gridGenerator();
+changeColor();
